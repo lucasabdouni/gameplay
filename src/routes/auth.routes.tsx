230ddaf65e722b3e0eb4465b93009ba778cdb005
Lucas <lucasabdouni@hotmail.com>
2021-11-15 12:25:27 -0300

    added Home page

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,30 +1,26 @@
 import React from 'react';
 
-import {
-  createNativeStackNavigator,
-  NativeStackNavigationOptions,
-} from 'react-native-screens/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '../screens/Home';
 import { SignIn } from '../screens/SignIn';
 
-const Stack = createNativeStackNavigator();
+import { theme } from '../global/styles/theme';
+
+const { Navigator, Screen } = createStackNavigator();
 
 export function AuthRoutes() {
   return (
-    <Stack.Navigator screenOptions={getScreenOptions()}>
-      <Stack.Group>
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Group>
-    </Stack.Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: theme.colors.secondary100,
+        },
+      }}
+    >
+      <Screen name="SignIn" component={SignIn} />
+      <Screen name="Home" component={Home} />
+    </Navigator>
   );
-}
-
-function getScreenOptions(): NativeStackNavigationOptions {
-  return {
-    contentStyle: {
-      backgroundColor: 'transparent',
-    },
-  };
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+import { Appointment } from '../../components/Appointment';
+
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { CategorySelect } from '../../components/CategorySelect';
+import { ListDivider } from '../../components/ListDivider';
+import { ListHeader } from '../../components/ListHeader';
 import { Profile } from '../../components/Profile';
 
 import { styles } from './styles';
 
 export function Home() {
+  const [category, setCategory] = useState('');
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vai rolar um pvp de maluco',
+    },
+  ];
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Profile />
+        <ButtonAdd />
+      </View>
+
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+
+      <View style={styles.content}>
+        <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+
+        <FlatList
+          data={appointments}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
     </View>
   );
